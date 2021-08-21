cd7c674bb1d3a52bdf92293a0e0a482d3040ec79
eternal <73202350+slow@users.noreply.github.com>
2021-08-21 05:51:23 +0100

    Update downloader.js

--- downloader.js
+++ downloader.js
@@ -1,7 +1,8 @@
 window.DownloadApi = {
    converter: {
       plugin: arg => `https://raw.githubusercontent.com/slow/better-discord-plugins/master/${arg}/${arg}.plugin.js`,
-      theme: arg => `https://raw.githubusercontent.com/slow/${arg}/master/${arg.split('-').map(a => `${a[0].toUpperCase()}${a.slice(1)}`).join('')}.theme.css`
+      theme: arg => `https://raw.githubusercontent.com/slow/${arg}/master/${arg.split('-').map(a => `${a[0].toUpperCase()}${a.slice(1)}`).join('')}.theme.css`,
+      custom: arg => arg;
    },
    convert: (parameterString, error) => {
       if (typeof parameterString == 'string') for (let parameter in window.DownloadApi.converter) {
